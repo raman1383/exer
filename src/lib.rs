c8e527989883f1c77551577f0e404eee570a3310
raman1383 <raman83shariati@gmail.com>
2022-05-14 16:23:50 +0430

    r

--- src/lib.rs
+++ src/lib.rs
@@ -73,3 +73,32 @@ mod test {
         assert_eq!(Some(3), binary_search(&mut [12, 3, 2, 46, 7, 8], 7, &46));
     }
 }
+
+// use rand::Rng;
+// use std::cmp::Ordering;
+// use std::io;
+// fn main() {
+//     println!("guess a number...");
+//     let secret_number = rand::thread_rng().gen_range(1..101);
+//     print!("Now, Enter youe guess: \n");
+//     loop {
+//         let mut guess = String::new();
+//         io::stdin()
+//             .read_line(&mut guess)
+//             .expect("Unable to read the input !");
+//         let guess: i32 = match guess.trim().parse() {
+//             Ok(guess) => guess,
+//             Err(_) => continue,
+//         };
+//         match guess.cmp(&secret_number) {
+//             Ordering::Greater => println!("come lower..."),
+//             Ordering::Less => println!("go higher..."),
+//             Ordering::Equal => {
+//                 println!("😍😍😍😍😍😍😍😍");
+//                 println!("    You Win      ");
+//                 println!("😍😍😍😍😍😍😍😍");
+//                 break;
+//             }
+//         }
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,10 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
 fn main() {
-    println!("guess a number...");
-    let secret_number = rand::thread_rng().gen_range(1..101);
-    print!("Now, Enter youe guess: \n");
-    loop {
-        let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Unable to read the input !");
-        let guess: i32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        match guess.cmp(&secret_number) {
-            Ordering::Greater => println!("come lower..."),
-            Ordering::Less => println!("go higher..."),
-            Ordering::Equal => {
-                println!("😍😍😍😍😍😍😍😍");
-                println!("    You Win      ");
-                println!("😍😍😍😍😍😍😍😍");
-                break;
-            }
-        }
-    }
+    let x = 1;
+    let x = x + 1;
+    let x = x + 1;
+    println!("{x}"); // 3
+
+    let space = "     ";
+    let space = space.len();
+    println!("{space}"); // 5
 }
