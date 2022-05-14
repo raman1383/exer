7e277bdd93f537981c0392aa9b9368116fe1035a
raman1383 <raman83shariati@gmail.com>
2022-05-14 15:17:13 +0430

    rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("hello world");
+    println!("guess a number...");
+    print!("Now, Enter youe guess: ");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Unable to read the input !");
+    println!("your guess is ->  {guess}");
 }
