7da0e07b2d8568e85a888673638a4dc317953e1a
raman1383 <raman83shariati@gmail.com>
2022-05-14 20:05:28 +0430

    rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
-fn main() {
-    let x = 1;
-    let x = x + 1;
-    let x = x + 1;
-    println!("{x}"); // 3
+#![allow(unused)]
 
-    let space = "     ";
-    let space = space.len();
-    println!("{space}"); // 5
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+fn main() {
+    const PI: f32 = 3.1415;
+    println!("guess a number...");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    print!("Now, Enter youe guess: \n");
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Unable to read the input !");
+        let guess: i32 = match guess.trim().parse() {
+            Ok(guess) => guess,
+            Err(_) => continue,
+        };
+        match guess.cmp(&secret_number) {
+            Ordering::Greater => println!("come lower..."),
+            Ordering::Less => println!("go higher..."),
+            Ordering::Equal => {
+                println!("😍😍😍😍😍😍😍😍");
+                println!("    You Win      ");
+                println!("😍😍😍😍😍😍😍😍");
+                break;
+            }
+        }
+    }
 }
