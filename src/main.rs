3d6fe24ae8d1e697b5a552a11757aeef1880cb3d
raman1383 <raman83shariati@gmail.com>
2022-05-17 07:43:33 +0430

    r

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,49 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
+// use rand::Rng;
+// use std::cmp::Ordering;
+// use std::io;
 
 fn main() {
-    println!("guess a number...");
-    let secret_number = rand::thread_rng().gen_range(1..101);
-    print!("Now, Enter youe guess: \n");
-    loop {
-        let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Unable to read the input !");
-        let guess: i32 = match guess.trim().parse() {
-            Ok(guess) => guess,
-            Err(_) => continue,
-        };
-        match guess.cmp(&secret_number) {
-            Ordering::Greater => println!("come lower..."),
-            Ordering::Less => println!("go higher..."),
-            Ordering::Equal => {
-                println!("✌️ You Win ✌️");
-                break;
-            }
+    // println!("guess a number...");
+    // let secret_number = rand::thread_rng().gen_range(1..101);
+    // print!("Now, Enter youe guess: \n");
+    // loop {
+    //     let mut guess = String::new();
+    //     io::stdin()
+    //         .read_line(&mut guess)
+    //         .expect("Unable to read the input !");
+    //     let guess: i32 = match guess.trim().parse() {
+    //         Ok(guess) => guess,
+    //         Err(_) => continue,
+    //     };
+    //     match guess.cmp(&secret_number) {
+    //         Ordering::Greater => println!("come lower..."),
+    //         Ordering::Less => println!("go higher..."),
+    //         Ordering::Equal => {
+    //             println!("✌️ You Win ✌️");
+    //             break;
+    //         }
+    //     }
+    // }
+
+    let words = String::from("Hello world");
+    let fist_word_in_words = first_word(&words);
+    println!("{}", fist_word_in_words);
+    
+    let s = "hello "; // stacked
+    let ss = s;
+    println!("{}{}",s,ss);
+
+    let st = String::from("hello "); // heaped
+    let _stt = st;
+    // println!("{}{}", st,stt);
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
         }
     }
-
-    let tuple = (12, 4, 54, 77);
-    let array = [12, 33, 44, 5, 67];
-    let vec = vec![12, 3, 44, 55, 77, 23];
-
-    let tuple_2 = tuple;
-    println!("tuple -> {:?}, tuple_2 -> {:?}", tuple, tuple_2);
-
-    let array_2 = array;
-    println!("array_2 -> {:?}, array -> {:?}", array_2, array);
-
-    let vec_2 = vec;r
-    println!("vec_2 -> {:?}, vec -> {:?}", vec_2, vec);
+    &s[..]
 }
