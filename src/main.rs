fa03ee4ab6b571980ea629b548308fd4102adb4d
raman1383 <raman83shariati@gmail.com>
2022-05-17 09:01:30 +0430

    r

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,11 @@
-// use rand::Rng;
-// use std::cmp::Ordering;
-// use std::io;
-
 fn main() {
-    // println!("guess a number...");
-    // let secret_number = rand::thread_rng().gen_range(1..101);
-    // print!("Now, Enter youe guess: \n");
-    // loop {
-    //     let mut guess = String::new();
-    //     io::stdin()
-    //         .read_line(&mut guess)
-    //         .expect("Unable to read the input !");
-    //     let guess: i32 = match guess.trim().parse() {
-    //         Ok(guess) => guess,
-    //         Err(_) => continue,
-    //     };
-    //     match guess.cmp(&secret_number) {
-    //         Ordering::Greater => println!("come lower..."),
-    //         Ordering::Less => println!("go higher..."),
-    //         Ordering::Equal => {
-    //             println!("✌️ You Win ✌️");
-    //             break;
-    //         }
-    //     }
-    // }
-
     let words = String::from("Hello world");
     let fist_word_in_words = first_word(&words);
     println!("{}", fist_word_in_words);
-    
+
     let s = "hello "; // stacked
     let ss = s;
-    println!("{}{}",s,ss);
+    println!("{}{}", s, ss);
 
     let st = String::from("hello "); // heaped
     let _stt = st;
