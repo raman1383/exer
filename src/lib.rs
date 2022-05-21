b39c2f1a0fb862e2c2a933807d031afc89a98cb0
raman1383 <raman83shariati@gmail.com>
2022-05-21 13:37:13 +0430

    r

--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,65 @@
-fn partition<T: PartialOrd>(arr: &mut [T], lo: isize, hi: isize) -> isize {
-    let pivot = hi as usize;
-    let mut i = lo - 1;
-    let mut j = hi;
-    loop {
-        i += 1;
-        while arr[i as usize] < arr[pivot as usize] {
-            i += 1
-        }
-        j -= 1;
-        while j >= 0 && arr[j as usize] > arr[pivot as usize] {
-            j -= 1
-        }
-        if i >= j {
-            break;
-        } else {
-            arr.swap(i as usize, j as usize);
-        }
+pub mod quick_sort {
+    pub fn quick_sort<T: Ord>(arr: &mut [T]) -> &mut [T] {
+        let len = arr.len();
+        _quick_sort(arr, 0, (len - 1) as isize)
     }
-    arr.swap(i as usize, pivot as usize);
-    i
-}
-
-fn _quick_sort<T: Ord>(arr: &mut [T], lo: isize, hi: isize) -> &mut [T] {
-    if lo < hi {
-        let pivot_point = partition(arr, lo, hi);
-        _quick_sort(arr, lo, pivot_point - 1);
-        _quick_sort(arr, pivot_point + 1, hi);
+    fn partition<T: PartialOrd>(arr: &mut [T], lo: isize, hi: isize) -> isize {
+        let pivot = hi as usize;
+        let mut i = lo - 1;
+        let mut j = hi;
+        loop {
+            i += 1;
+            while arr[i as usize] < arr[pivot as usize] {
+                i += 1
+            }
+            j -= 1;
+            while j >= 0 && arr[j as usize] > arr[pivot as usize] {
+                j -= 1
+            }
+            if i >= j {
+                break;
+            } else {
+                arr.swap(i as usize, j as usize);
+            }
+        }
+        arr.swap(i as usize, pivot as usize);
+        i
     }
-    arr
-}
 
-pub fn quick_sort<T: Ord>(arr: &mut [T]) -> &mut [T] {
-    let len = arr.len();
-    _quick_sort(arr, 0, (len - 1) as isize)
+    fn _quick_sort<T: Ord>(arr: &mut [T], lo: isize, hi: isize) -> &mut [T] {
+        if lo < hi {
+            let pivot_point = partition(arr, lo, hi);
+            _quick_sort(arr, lo, pivot_point - 1);
+            _quick_sort(arr, pivot_point + 1, hi);
+        }
+        arr
+    }
 }
 
 // Binary search
-pub fn binary_search(arr: &[i32], length: usize, target: &i32) -> Option<usize> {
-    let mut low = 0;
-    let mut high = length - 1;
-    while low < high {
-        let mid = ((high + low) / 2) + low;
-        let mid_index = mid;
-        let value = &arr[mid_index];
+pub mod binary_search {
+    pub fn binary_search(arr: &[i32], length: usize, target: &i32) -> Option<usize> {
+        let mut low = 0;
+        let mut high = length - 1;
+        while low < high {
+            let mid = ((high + low) / 2) + low;
+            let mid_index = mid;
+            let value = &arr[mid_index];
 
-        if value == target {
-            return Some(mid_index);
-        }
+            if value == target {
+                return Some(mid_index);
+            }
 
-        if value < target {
-            low = mid + 1;
-        }
+            if value < target {
+                low = mid + 1;
+            }
 
-        if value > target {
-            high = mid - 1;
+            if value > target {
+                high = mid - 1;
+            }
         }
+        None
     }
-    None
 }
 
 #[cfg(test)]
@@ -65,12 +68,15 @@ mod test {
     #[test]
     fn q_sort() {
         let mut arr = [2, 3, 5, 4, 1];
-        assert_eq!(quick_sort(&mut arr), [1, 2, 3, 4, 5])
+        assert_eq!(quick_sort::quick_sort(&mut arr), [1, 2, 3, 4, 5])
     }
 
     #[test]
     fn bin_search() {
-        assert_eq!(Some(3), binary_search(&mut [12, 3, 2, 46, 7, 8], 7, &46));
+        assert_eq!(
+            Some(3),
+            binary_search::binary_search(&mut [12, 3, 2, 46, 7, 8], 7, &46)
+        );
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Read;
+
 fn main() {
     let words = String::from("Hello world");
     let fist_word_in_words = first_word(&words);
@@ -34,7 +36,29 @@ fn main() {
     let x: i8 = 5;
     let y: Option<i8> = Some(5);
     let sum = x + y.unwrap();
-    println!("{sum}")
+    println!("{sum}");
+
+    let xx: i8 = 5;
+    let yy: Result<i8, &str> = Ok(12);
+    let sum = xx + yy.unwrap();
+    println!("{sum}");
+
+    let coin_1 = Coin::Quarter(UsState::Alabama);
+    println!("{:?}", coin_1);
+
+    let mut arr: [i32; 6] = [23, 4, 312, 67, 243, 876];
+    let y = exer::binary_search::binary_search(&arr, 6, &312);
+    println!("{:?}", y);
+    let x = exer::quick_sort::quick_sort(&mut arr);
+    println!("{:?}", x);
+
+    let mut vec = Vec::<i32>::new();
+    vec.push(33);
+    vec.push(22);
+    vec.pop();
+
+    let mut hash_map = std::collections::HashMap::<&str, i32>::new();
+    hash_map.insert("Leakers", 122);
 }
 
 fn first_word(s: &String) -> &str {
@@ -58,4 +82,27 @@ impl Person {
     fn new(name: String, age: u8, alive: bool) -> Person {
         Person { name, age, alive }
     }
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn _read_username_from_file() -> Result<String, std::io::Error> {
+    let mut s = String::new();
+    std::fs::File::open("hello.txt")?.read_to_string(&mut s)?; // WOW !
+    Ok(s)
 }
