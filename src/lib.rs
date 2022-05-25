1412db99c53c8f21a9f3642317b376160d4d15c4
raman1383 <raman83shariati@gmail.com>
2022-05-25 12:03:23 +0430

    -change_coin

--- src/lib.rs
+++ src/lib.rs
@@ -1,139 +1,137 @@
-pub mod quick_sort {
-    pub fn quick_sort<T: Ord>(arr: &mut [T]) -> &mut [T] {
-        let len = arr.len();
-        _quick_sort(arr, 0, (len - 1) as isize)
-    }
-    fn partition<T: PartialOrd>(arr: &mut [T], lo: isize, hi: isize) -> isize {
-        let pivot = hi as usize;
-        let mut i = lo - 1;
-        let mut j = hi;
-        loop {
-            i += 1;
-            while arr[i as usize] < arr[pivot as usize] {
-                i += 1
-            }
-            j -= 1;
-            while j >= 0 && arr[j as usize] > arr[pivot as usize] {
-                j -= 1
-            }
-            if i >= j {
-                break;
-            } else {
-                arr.swap(i as usize, j as usize);
-            }
-        }
-        arr.swap(i as usize, pivot as usize);
-        i
-    }
+pub fn quick_sort<T: Ord>(arr: &mut [T]) -> &mut [T] {
+    let len = arr.len();
+    _quick_sort(arr, 0, (len - 1) as isize)
+}
 
-    fn _quick_sort<T: Ord>(arr: &mut [T], lo: isize, hi: isize) -> &mut [T] {
-        if lo < hi {
-            let pivot_point = partition(arr, lo, hi);
-            _quick_sort(arr, lo, pivot_point - 1);
-            _quick_sort(arr, pivot_point + 1, hi);
+fn partition<T: PartialOrd>(arr: &mut [T], lo: isize, hi: isize) -> isize {
+    let pivot = hi as usize;
+    let mut i = lo - 1;
+    let mut j = hi;
+    loop {
+        i += 1;
+        while arr[i as usize] < arr[pivot as usize] {
+            i += 1
         }
-        arr
+        j -= 1;
+        while j >= 0 && arr[j as usize] > arr[pivot as usize] {
+            j -= 1
+        }
+        if i >= j {
+            break;
+        } else {
+            arr.swap(i as usize, j as usize);
+        }
+    }
+    arr.swap(i as usize, pivot as usize);
+    i
+}
+
+fn _quick_sort<T: Ord>(arr: &mut [T], lo: isize, hi: isize) -> &mut [T] {
+    if lo < hi {
+        let pivot_point = partition(arr, lo, hi);
+        _quick_sort(arr, lo, pivot_point - 1);
+        _quick_sort(arr, pivot_point + 1, hi);
+    }
+    arr
+}
+
+pub fn binary_search(arr: &[i32], length: usize, target: &i32) -> Option<usize> {
+    let mut low = 0;
+    let mut high = length - 1;
+    while low < high {
+        let mid = ((high + low) / 2) + low;
+        let mid_index = mid;
+        let value = &arr[mid_index];
+
+        if value == target {
+            return Some(mid_index);
+        }
+
+        if value < target {
+            low = mid + 1;
+        }
+
+        if value > target {
+            high = mid - 1;
+        }
+    }
+    None
+}
+
+use std::collections::HashMap;
+
+pub fn memoized_fibonacci(n: u32) -> u128 {
+    let mut cache: HashMap<u32, u128> = HashMap::new();
+    _memoized_fibonacci(n, &mut cache)
+}
+
+fn _memoized_fibonacci(n: u32, cache: &mut HashMap<u32, u128>) -> u128 {
+    if n == 0 {
+        return 0;
+    }
+    if n == 1 {
+        return 1;
+    }
+    let f = match cache.get(&n) {
+        Some(f) => f,
+        None => {
+            let f1 = _memoized_fibonacci(n - 1, cache);
+            let f2 = _memoized_fibonacci(n - 2, cache);
+            cache.insert(n, f1 + f2);
+            cache.get(&n).unwrap()
+        }
+    };
+    *f
+}
+
+pub fn coin_change(coins: &[usize], amount: usize) -> Option<usize> {
+    let mut dp = vec![std::usize::MAX; amount + 1];
+    dp[0] = 0;
+
+    // Assume dp[i] is the fewest number of coins making up amount i,
+    // then for every coin in coins, dp[i] = min(dp[i - coin] + 1).
+    for i in 0..=amount {
+        for j in 0..coins.len() {
+            if i >= coins[j] && dp[i - coins[j]] != std::usize::MAX {
+                dp[i] = dp[i].min(dp[i - coins[j]] + 1);
+            }
+        }
+    }
+    match dp[amount] {
+        std::usize::MAX => None,
+        _ => Some(dp[amount]),
     }
 }
 
-// Binary search
-pub mod binary_search {
-    pub fn binary_search(arr: &[i32], length: usize, target: &i32) -> Option<usize> {
-        let mut low = 0;
-        let mut high = length - 1;
-        while low < high {
-            let mid = ((high + low) / 2) + low;
-            let mid_index = mid;
-            let value = &arr[mid_index];
-
-            if value == target {
-                return Some(mid_index);
-            }
-
-            if value < target {
-                low = mid + 1;
-            }
-
-            if value > target {
-                high = mid - 1;
-            }
-        }
-        None
-    }
-}
+pub fn two_sum() {}
 
 #[cfg(test)]
 mod test {
     use crate::*;
     #[test]
-    fn q_sort() {
-        let mut arr = [2, 3, 5, 4, 1];
-        assert_eq!(quick_sort::quick_sort(&mut arr), [1, 2, 3, 4, 5])
+    fn bin_search() {
+        assert_eq!(Some(3), binary_search(&mut [12, 3, 2, 46, 7, 8], 7, &46));
     }
 
     #[test]
-    fn bin_search() {
-        assert_eq!(
-            Some(3),
-            binary_search::binary_search(&mut [12, 3, 2, 46, 7, 8], 7, &46)
-        );
-    }
-}
-
-// use rand::Rng;
-// use std::cmp::Ordering;
-// use std::io;
-// fn main() {
-//     println!("guess a number...");
-//     let secret_number = rand::thread_rng().gen_range(1..101);
-//     print!("Now, Enter you guess: \n");
-//     loop {
-//         let mut guess = String::new();
-//         io::stdin()
-//             .read_line(&mut guess)
-//             .expect("Unable to read the input !");
-//         let guess: i32 = match guess.trim().parse() {
-//             Ok(guess) => guess,
-//             Err(_) => continue,
-//         };
-//         match guess.cmp(&secret_number) {
-//             Ordering::Greater => println!("come lower..."),
-//             Ordering::Less => println!("go higher..."),
-//             Ordering::Equal => {
-//                 println!("😍😍😍😍😍😍😍😍");
-//                 println!("    You Win      ");
-//                 println!("😍😍😍😍😍😍😍😍");
-//                 break;
-//             }
-//         }
-//     }
-// }
-
-// Fibonacci with dynamic programming
-pub mod fibonacci {
-    use std::collections::HashMap;
-
-    pub fn memoized_fibonacci(n: u32) -> u128 {
-        let mut cache: HashMap<u32, u128> = HashMap::new();
-        _memoized_fibonacci(n, &mut cache)
+    fn q_sort() {
+        let mut arr = [2, 3, 5, 4, 1];
+        assert_eq!(quick_sort(&mut arr), [1, 2, 3, 4, 5])
     }
 
-    fn _memoized_fibonacci(n: u32, cache: &mut HashMap<u32, u128>) -> u128 {
-        if n == 0 {
-            return 0;
-        }
-        if n == 1 {
-            return 1;
-        }
-        let f = match cache.get(&n) {
-            Some(f) => f,
-            None => {
-                let f1 = _memoized_fibonacci(n - 1, cache);
-                let f2 = _memoized_fibonacci(n - 2, cache);
-                cache.insert(n, f1 + f2);
-                cache.get(&n).unwrap()
-            }
-        };
-        *f
+    #[test]
+    fn test_fibonacci() {
+        assert_eq!(memoized_fibonacci(10), 55);
+        assert_eq!(memoized_fibonacci(20), 6765);
+    }
+
+    #[test]
+    fn test_coin_change() {
+        let coins = vec![1, 2, 5];
+        assert_eq!(Some(3), coin_change(&coins, 11));
+
+        // 119 = 11 * 10 + 7 * 1 + 2 * 1
+        let coins = vec![2, 3, 5, 7, 11];
+        assert_eq!(Some(12), coin_change(&coins, 119));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use exer::fibonacci::memoized_fibonacci;
+use exer::coin_change;
+use exer::memoized_fibonacci;
 
 fn main() {
-    println!("{}", memoized_fibonacci(6))
+    println!("{}", memoized_fibonacci(6));
+    println!("{}", coin_change(&[], 2).unwrap());
 }
