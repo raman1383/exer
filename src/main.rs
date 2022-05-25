3dc7197cb77bb60832a120ebf56a47fe8efe0f8f
raman1383 <raman83shariati@gmail.com>
2022-05-25 21:52:43 +0430

    rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+use std::collections::HashMap;
+
 use exer::coin_change;
 use exer::memoized_fibonacci;
 
 fn main() {
     println!("{}", memoized_fibonacci(6));
-    println!("{}", coin_change(&[], 2).unwrap());
+    println!("{}", coin_change(&[12,2,1,1,3], 2).unwrap());
+
+    let mut hash_map = HashMap::new();
+    hash_map.insert(0, "zero");
+    hash_map.insert(1, "ONE");
+    hash_map.insert(2, "TWO");
+    hash_map.insert(3, "THREE");
+
+    println!("{:?}", hash_map.get(&2));
 }
