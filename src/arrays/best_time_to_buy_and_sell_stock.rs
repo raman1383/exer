6f06ffaff8934a54c9f1b7d6bc2e694845d27bda
raman1383 <raman83shariati@gmail.com>
2022-05-26 15:21:48 +0430

    rr

--- src/arrays/best_time_to_buy_and_sell_stock.rs
+++ src/arrays/best_time_to_buy_and_sell_stock.rs
@@ -1,3 +1,11 @@
+//You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+//You want to maximize your profit by choosing a single day to buy one stock and choosing
+//a different day in the future to sell that stock.
+
+//Return the maximum profit you can achieve from this transaction. If you cannot achieve
+// any profit, return 0.
+
 pub mod best_time_to_buy_and_sell_stock {
     pub fn _best_time_to_buy_and_sell_stock(prices: &Vec<i32>) -> i32 {
         let mut min_price = prices[0];
--- src/arrays/two_sum.rs
+++ src/arrays/two_sum.rs
@@ -1,6 +1,12 @@
-pub mod two_sum {
+//Given an array of integers nums and an integer target, return indices of the two numbers 
+//such that they add up to target.
 
-    // One-pass Hash Table
+//You may assume that each input would have exactly one solution, and you may not use the 
+//same element twice.
+
+//You can return the answer in any order.
+
+pub mod two_sum {
     use std::{collections::HashMap, vec};
     pub fn _two_sum_one_pass(nums: &Vec<i32>, target: i32) -> Vec<i32> {
         let mut hash_map = HashMap::<i32, i32>::new();
@@ -13,3 +19,4 @@ pub mod two_sum {
         vec![]
     }
 }
+// [ 1,3,5]
