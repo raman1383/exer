3827fb42bb9326faafc60288507d2a66522191ab
raman1383 <raman83shariati@gmail.com>
2022-05-26 19:55:22 +0430

    rs

---
+++ src/arrays/max_product_subarray.rs
@@ -0,0 +1,8 @@
+//Given an integer array nums, find a contiguous non-empty subarray within the array that 
+//has the largest product, and return the product.
+
+
+
+pub mod max_product_subarray {
+    // pub fn max_product(nums: Vec<i32>) -> i32 {}
+}
