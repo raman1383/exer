c51bec9a6ead193a97702cb5781b397b22e1b2f4
raman1383 <raman83shariati@gmail.com>
2022-05-28 23:25:55 +0430

    f

--- src/arrays/top_k_frequent_elements.rs
+++ src/arrays/top_k_frequent_elements.rs
@@ -1,6 +1,22 @@
 pub mod top_k_frequent_elements {
-    pub fn top_k_frequent_elements(nums: Vec<i32>, k: i32) -> Vec<i32> {
-        
-        vec![]
+    pub fn _top_k_frequent_elements(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        let k = k as usize;
+        let n = nums.len();
+        let mut counter = std::collections::HashMap::<i32, i32>::with_capacity(n);
+        for &num in &nums {
+            *counter.entry(num).or_default() += 1;
+        }
+        let mut heap = std::collections::BinaryHeap::<(i32, i32)>::with_capacity(k);
+        for (&num, &cnt) in &counter {
+            if heap.len() == k {
+                if -cnt < heap.peek().unwrap().0 {
+                    heap.pop();
+                    heap.push((-cnt, num));
+                }
+            } else {
+                heap.push((-cnt, num));
+            }
+        }
+        heap.into_iter().rev().map(|(_, num)| num).collect()
     }
 }
