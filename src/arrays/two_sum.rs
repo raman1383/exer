08fbdd25ff60b6f46e39081ec90247522a406197
raman1383 <raman83shariati@gmail.com>
2022-05-30 22:14:08 +0430

    r

--- src/arrays/two_sum.rs
+++ src/arrays/two_sum.rs
@@ -6,19 +6,18 @@
 
 //You can return the answer in any order.
 
-pub mod two_sum {
-    pub fn _two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut hm = std::collections::HashMap::<i32, i32>::new();
-        for (i, num) in nums.iter().enumerate() {
-            match hm.get(&num) {
-                Some(&index) => return vec![index, i as i32],
-                None => hm.insert(target - num, i as i32),
-            };
-        }
-
-        vec![]
+pub fn _two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut hm = std::collections::HashMap::<i32, i32>::new();
+    for (i, num) in nums.iter().enumerate() {
+        match hm.get(&num) {
+            Some(&index) => return vec![index, i as i32],
+            None => hm.insert(target - num, i as i32),
+        };
     }
+
+    vec![]
 }
+
 // [ 0:1, 1:3, 2:5] t:8
 
 // hash_map[7:0, 5:1, 3:2]
