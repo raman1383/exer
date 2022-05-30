feacbbf3532fdc3f661e8e83716507fde00da775
raman1383 <raman83shariati@gmail.com>
2022-05-30 22:12:35 +0430

    r

--- src/arrays/product_of_array_except_self.rs
+++ src/arrays/product_of_array_except_self.rs
@@ -3,13 +3,12 @@ pub mod product_of_array_except_self {
         let mut top_right = 1;
         let mut bottom_left = 1;
 
-        let n = nums.len();
-        let mut ret = vec![1; n];
-        for i in (0..n).rev() {
+        let mut ret = vec![1; nums.len()];
+        for i in (0..nums.len()).rev() {
             ret[i] = ret[i] * top_right;
             top_right = top_right * nums[i];
         }
-        for i in 0..n {
+        for i in 0..nums.len() {
             ret[i] = ret[i] * bottom_left;
             bottom_left = bottom_left * nums[i];
         }
