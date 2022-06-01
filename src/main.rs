ef3906c0f03236e5c0946e648f4b68b04bd483e9
raman1383 <raman83shariati@gmail.com>
2022-06-01 21:17:41 +0430

    r

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-fn main() {
+use std::process::ExitCode;
+
+fn main() -> ExitCode {
     let x = 1;
     let x = x + 2;
     let x = x * 2;
     println!("Value of x: {}", x);
+
+    ExitCode::SUCCESS
 }
