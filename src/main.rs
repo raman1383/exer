49d471ece17846c9444a05de4f910d0b9cd42c32
raman1383 <raman83shariati@gmail.com>
2022-06-04 19:16:36 +0430

    r

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,11 @@
-fn main() {}
+fn main() {
+    println!();
+    let mut x = 0;
+    loop {
+        x += 1;
+        println!("hello world");
+        if x == 100 {
+            break;
+        }
+    }
+}
