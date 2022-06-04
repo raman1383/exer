b90573886d11a8550eb165f9bb632f711d9e0508
raman1383 <raman83shariati@gmail.com>
2022-06-04 14:24:25 +0430

    r

--- src/sort/quick_sort.rs
+++ src/sort/quick_sort.rs
@@ -3,4 +3,33 @@ pub fn quick_sort<T: Ord>(arr: &mut [T]) {
     _quick_sort(arr, 0, (len - 1) as isize)
 }
 
-fn _quick_sort<T: Ord>(arr: &mut [T], low: isize, high: isize) {}
+fn _quick_sort<T: Ord>(arr: &mut [T], low: isize, high: isize) {
+    if low < high {
+        let p = partition(arr, low, high);
+        _quick_sort(arr, low, p - 1);
+        _quick_sort(arr, p + 1, high);
+    }
+}
+
+fn partition<T: Ord>(arr: &mut [T], low: isize, high: isize) -> isize {
+    let pivot = high as usize;
+    let mut i = low - 1;
+    let mut j = high;
+    loop {
+        i += 1;
+        while arr[i as usize] < arr[pivot] {
+            i += 1;
+        }
+        j -= 1;
+        while j >= 0 && arr[j as usize] > arr[pivot] {
+            j -= 1;
+        }
+        if i >= j {
+            break;
+        } else {
+            arr.swap(i as usize, j as usize)
+        }
+    }
+    arr.swap(i as usize, pivot as usize);
+    i
+}
