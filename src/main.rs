bcda0add0336f21a94e226791f06813d6001d036
raman1383 <raman83shariati@gmail.com>
2022-06-04 18:49:49 +0430

    r

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,1 @@
-fn main() {
-    let x = 1;
-    let x = x + 2;
-    let x = x * 2;
-    println!("Value of x: {}", x);
-
-    println!("{}", format!("{:.*}", 4, 12.1234));
-
-    let _arr2vec = [23, 3, 44, 8].to_vec();
-}
+fn main() {}
