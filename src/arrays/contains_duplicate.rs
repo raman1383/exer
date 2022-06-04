3416d68393c43612f788f93a547b3357b50de264
raman1383 <raman83shariati@gmail.com>
2022-06-04 19:17:16 +0430

    r

--- src/arrays/contains_duplicate.rs
+++ src/arrays/contains_duplicate.rs
@@ -1,15 +1,13 @@
 //Given an integer array nums, return true if any value appears at
 //least twice in the array, and return false if every element is distinct.
 
-pub mod contains_duplicate {
-    pub fn _contains_duplicate(nums: Vec<i32>) -> bool {
-        let mut hm = std::collections::HashMap::<i32, i32>::new();
-        for i in nums {
-            match hm.contains_key(&i) {
-                true => return true,
-                false => hm.insert(i, i),
-            };
-        }
-        false
+pub fn _contains_duplicate(nums: Vec<i32>) -> bool {
+    let mut hm = std::collections::HashMap::<i32, i32>::new();
+    for i in nums {
+        match hm.contains_key(&i) {
+            true => return true,
+            false => hm.insert(i, i),
+        };
     }
+    false
 }
