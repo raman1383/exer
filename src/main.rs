a77608a036e95c4b37db09e4854f02723359b1b8
raman1383 <raman83shariati@gmail.com>
2022-06-07 18:29:43 +0430

    f

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 #![allow(unused)]
-
 fn main() {
-    for i in (0..6).rev() {
-        for j in 0..i {
+    let arr = [1, 2, 3, 4, 3, 2, 1];
+    for i in 0..7 {
+        for j in 0..arr[i] {
             print!("#")
+        }
+        print!("\n")
+    }
+
+    println!("-------------");
+
+    for mut i in 0..8 {
+        if i <= 4 {
+            for j in 0..i {
+                print!("#")
+            }
+        } else {
+            for j in 0..(8 - i) {
+                print!("#")
+            }
         }
         print!("\n")
     }
