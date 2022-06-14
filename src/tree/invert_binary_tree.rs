5ae73e18130c4f894dab1a8f3912d7120f321d60
raman1383 <raman83shariati@gmail.com>
2022-06-14 18:49:09 +0430

    r

--- src/tree/invert_binary_tree.rs
+++ src/tree/invert_binary_tree.rs
@@ -1,3 +1,21 @@
-// problem:
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
 
-pub fn _invert_binary_tree() {}
+impl TreeNode {
+    #[inline]
+    pub fn _new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+use std::cell::RefCell;
+use std::rc::Rc;
+// pub fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {}
