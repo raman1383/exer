d5fb187e48433c737d900c4818ab58d462440b49
raman1383 <raman83shariati@gmail.com>
2022-06-15 10:36:11 +0430

    r

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
+mod arrays;
+mod dyn_pro;
 mod search;
-
-fn main() {
-    let x = 10;
-    println!("hello {x} times ");
-
-    println!(
-        "{:?}",
-        search::binary_search::_binary_search(&[12, 33, 44, 56, 87, 237, 678], 87)
-    );
-
-    println!("The grand empire of software...")
-}
+mod sort;
+fn main() {}
