b670155396d49120efe8999bc91f08725e258b20
raman1383 <raman83shariati@gmail.com>
2022-06-16 13:35:33 +0430

    r

--- src/search/linear_search.rs
+++ src/search/linear_search.rs
@@ -1,4 +1,4 @@
-pub fn linear_search<T: PartialEq>(item: &T, arr: &[T]) -> Option<usize> {
+pub fn linear_search<T: PartialEq>(arr: &[T], item: &T) -> Option<usize> {
     for (i, data) in arr.iter().enumerate() {
         if item == data {
             return Some(i);
