37ca43090f84d36c53048e913d41840815cb4fb4
raman1383 <raman83shariati@gmail.com>
2022-06-22 12:03:03 +0430

    r

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,4 @@ mod arrays;
 mod dyn_pro;
 pub mod search;
 mod sort;
-fn main() {
-    let (cpp, rust) = ("cpp sucks", "rust is AWESOME");
-
-    for i in 0..100 {
-        println!(" {i} -> {cpp}, {rust}")
-    }
-}
+fn main() {}
