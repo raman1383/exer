8b7682840d5e8497e3879f9ac933f52713ff0d5b
raman1383 <raman83shariati@gmail.com>
2022-06-23 07:11:00 +0430

    r

--- src/dyn_pro/unique_paths.rs
+++ src/dyn_pro/unique_paths.rs
@@ -1,1 +1,15 @@
-// pub fn unique_paths(m: i32, n: i32) -> i32 {}
+pub fn unique_paths(m: i32, n: i32) -> i32 {
+    let m = m as usize;
+    let n = n as usize;
+    let mut a = vec![vec![0; m + 1]; n + 1];
+    for i in 1..=n {
+        for j in 1..=m {
+            if i == 1 && j == 1 {
+                a[i][j] = 1;
+            } else {
+                a[i][j] = a[i - 1][j] + a[i][j - 1];
+            }
+        }
+    }
+    a[n][m]
+}
